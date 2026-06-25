58e8168362f7f9b6b65cf54525d1af88a248ceea
Rafael da Silva Longo <rafael.longo97@gmail.com>
2026-06-25 13:27:24 -0300

    chore: add start.bat script

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+title Servidor Local Gelotin
+echo ===================================================
+echo   Iniciando o Servidor Local da Gelotin...
+echo   Acesse o site em: http://localhost:3000
+echo   Para encerrar o servidor, feche esta janela.
+echo ===================================================
+echo.
+node server.js
+pause
